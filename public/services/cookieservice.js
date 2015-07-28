48a294a7a1d1b16880dbcdb710ce058996255668
Sean Darcy <darcys22@gmail.com>
2015-07-28 17:56:10 +1000

    hashed the cookie

--- public/services/cookieservice.js
+++ public/services/cookieservice.js
@@ -1,12 +1,17 @@
 (function () {
 
+  var crypto = require('crypto');
+
   var injectParams = ['$cookies'];
 
   var cookieService = function ($cookies) {
     return {
 
       newCookie: function() {
-        var cookie = ("" + Date.now() + Math.floor(Math.random()*5000));
+        var nonce = ("" + Date.now() + Math.floor(Math.random()*5000));
+        var cookie = crypto.createHash("md5")
+          .update(nonce)
+          .digest("hex");
         $cookies.put('current.user', cookie)
         return cookie;
       }
