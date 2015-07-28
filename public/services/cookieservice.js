cd940784fab66a89ceb813e593f9e0cc9a3541b6
Sean Darcy <darcys22@gmail.com>
2015-07-28 18:26:13 +1000

    expiry date on cookies

--- public/services/cookieservice.js
+++ public/services/cookieservice.js
@@ -8,11 +8,13 @@
     return {
 
       newCookie: function() {
-        var nonce = ("" + Date.now() + Math.floor(Math.random()*5000));
+        var expireDate = new Date();
+        expireDate.setDate(expireDate.getDate() + 365 * 10);
+        var nonce = ("" + expireDate + Math.floor(Math.random()*5000));
         var cookie = crypto.createHash("md5")
           .update(nonce)
           .digest("hex");
-        $cookies.put('current.user', cookie)
+        $cookies.put('current.user', cookie, {'expires': expireDate})
         return cookie;
       }
 
