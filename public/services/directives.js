0ac63a2ba3cebd32f7b3705ee93aaa410b155440
Sean Darcy <darcys22@gmail.com>
2015-08-10 21:55:18 +1000

    images work now

--- public/services/directives.js
+++ public/services/directives.js
@@ -7,7 +7,6 @@
       link: function(scope, element, attrs) {
 
         element.bind("load", function(e){
-          alert("" + this.naturalHeight + this.naturalWidth);
           if(this.width > this.height) {
             img.width = '100%';
             img.height = 'auto';
