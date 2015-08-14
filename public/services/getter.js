dc64e4efac95b3013d89df3b9cde94d6c5699f92
Sean Darcy <darcys22@gmail.com>
2015-08-15 01:02:43 +1000

    fark broken, and confusting

--- public/services/getter.js
+++ public/services/getter.js
@@ -5,32 +5,37 @@
   var bookService = function ($http, $q, $filter) {
     var funcs = {
       apiTop: function() { $http.get('/api/top') },
-      getBooks: function() { $http.get('/api/users/' + userId + '/books') },
-      addBooks: function() {
+      getBooks: function(userId) { $http.get('/api/users/' + userId + '/books') },
+      addBooks: function(userId, bookId) { 
         $http.post('/api/users/' + userId + '/books',
             {'bookId': bookId})
       },
-      deleteBooks: function() { $http.delete('/api/users/' + userId + '/books/' + bookId) },
-      searchBooks: function() { $http.post('/api/search', JSON.stringify({'Keywords': query})) }
-    }
+      deleteBooks: function(userId, bookId) { $http.delete('/api/users/' + userId + '/books/' + bookId) },
+      addEmail: function(userId, email) { $http.post('/api/users/' + userId + '/email/', {'email': email} ) },
+      recoverBooks: function(email) { $http.post('/api/recover/', {'email': email}) },
+      searchBooks: function(query) { $http.post('/api/search', JSON.stringify({'Keywords': query})) }
+    };
 
-    var deferredbullshit = function(theFunctionActuallyWantingToCall) {
+    var deferredbullshit = function() {
+      var theFunction = arguments[0];
+      var args = Array.prototype.slice.call(arguments).pop();
+
       var deferred = $q.defer();
-      funcs[theFunctionActuallyWantingToCall]
+      funcs[theFunction].apply(this, args)
         .success(function(data) {
           deferred.resolve(data);
         }).error(function(data) {
           deferred.reject(data);
         });
       return deferred.promise;
-    }
+    };
 
     return {
-      getTopBooks: deferredbullshit('apiTop'),
-      getMyBooks: deferredbullshit('getBooks'),
-      addBookToUser: deferredbullshit('addBooks'),
-      deleteBookFromUser: deferredbullshit('deleteBooks'),
-      searchBooks: deferredbullshit('searchBooks')
+      getTopBooks: function() { deferredbullshit('apiTop') },
+      getMyBooks: function() { deferredbullshit('getBooks', arguments) },
+      addBookToUser: function() { deferredbullshit('addBooks', arguments) },
+      deleteBookFromUser: function() { deferredbullshit('deleteBooks', arguments) },
+      searchBooks: function() { deferredbullshit('searchBooks', arguments) }
     };
   };
 
