02bbd10cb636b60d8575a11bd4a6f914027e51e9
Sean Darcy <darcys22@gmail.com>
2015-08-15 05:36:29 +1000

    service isn't working, checked stackoverflow awaiting reply

--- public/services/getter.js
+++ public/services/getter.js
@@ -3,44 +3,80 @@
   var injectParams = ['$http', '$q', '$filter'];
 
   var bookService = function ($http, $q, $filter) {
-    var funcs = {
-      apiTop: function() { $http.get('/api/top') },
-      getBooks: function(userId) { $http.get('/api/users/' + userId + '/books') },
-      addBooks: function(userId, bookId) { 
+    return {
+
+      getTopBooks: function() {
+        var deferred = $q.defer();
+        $http.get('/api/top')
+          .success(function(data) {
+            deferred.resolve(data);
+          }).error(function(data) {
+            deferred.reject(data);
+          });
+          return deferred.promise;
+        },
+
+      getMyBooks: function(userId) {
+        var deferred = $q.defer();
+        $http.get('/api/users/' + userId + '/books')
+          .success(function(data) {
+            deferred.resolve(data);
+          }).error(function(data) {
+            deferred.reject(data);
+          });
+          return deferred.promise;
+        },
+
+      addBookToUser: function(userId, bookId) {
+        var deferred = $q.defer();
         $http.post('/api/users/' + userId + '/books',
             {'bookId': bookId})
-      },
-      deleteBooks: function(userId, bookId) { $http.delete('/api/users/' + userId + '/books/' + bookId) },
-      addEmail: function(userId, email) { $http.post('/api/users/' + userId + '/email/', {'email': email} ) },
-      recoverBooks: function(email) { $http.post('/api/recover/', {'email': email}) },
-      searchBooks: function(query) { $http.post('/api/search', JSON.stringify({'Keywords': query})) }
-    };
+          .success(function(data) {
+            deferred.resolve(data);
+          }).error(function(data) {
+            deferred.reject(data);
+          });
+          return deferred.promise;
+        },
 
-    var deferredbullshit = function() {
-      var theFunction = arguments[0];
-      var args = Array.prototype.slice.call(arguments).pop();
+      deleteBookFromUser: function(userId, bookId) {
+        var deferred = $q.defer();
+        $http.delete('/api/users/' + userId + '/books/' + bookId)
+          .success(function(data) {
+            deferred.resolve(data);
+          }).error(function(data) {
+            deferred.reject(data);
+          });
+          return deferred.promise;
+        },
 
-      var deferred = $q.defer();
-      funcs[theFunction].apply(this, args)
-        .success(function(data) {
-          deferred.resolve(data);
-        }).error(function(data) {
-          deferred.reject(data);
-        });
-      return deferred.promise;
-    };
+      searchBooks: function(query) {
+        var deferred = $q.defer();
+        $http.post('/api/search', JSON.stringify({'Keywords': query}))
+          .success(function(data) {
+            deferred.resolve(data);
+          }).error(function(data) {
+            deferred.reject(data);
+          });
+          return deferred.promise;
+        }
 
-    return {
-      getTopBooks: function() { deferredbullshit('apiTop') },
-      getMyBooks: function() { deferredbullshit('getBooks', arguments) },
-      addBookToUser: function() { deferredbullshit('addBooks', arguments) },
-      deleteBookFromUser: function() { deferredbullshit('deleteBooks', arguments) },
-      searchBooks: function() { deferredbullshit('searchBooks', arguments) }
+      }
     };
-  };
 
 
   bookService.$inject = injectParams;
 
   angular.module('myApp').factory('bookService', bookService);
 }());
+
+
+    //var doRequest = function(username, path) {
+      //return $http({
+        //method: 'JSONP',
+        //url: 'https://api.github.com/users/' + username + '/' + path + '?callback=JSON_CALLBACK'
+      //});
+    //}
+    //return {
+      //events: function(username) { return doRequest(username, 'events'); },
+    //};
